51efdf6f628ffca8225daa61082234756f2974e1
Gert Johannes Fourie <132646907+GJprocode@users.noreply.github.com>
2024-09-05 19:48:34 +0700

    deleted not needed be src file

--- src/src/types/express.d.ts
+++
@@ -1,9 +0,0 @@
-// types/express.d.ts or similar
-
-import { Request } from "express";
-
-declare module "express" {
-  export interface Request {
-    userId?: string; // Modify this if you use another field like `user`
-  }
-}
--- src/src/types/leo-profanity.d.ts
+++
@@ -1,8 +0,0 @@
-// declare module 'leo-profanity' {
-//     export function isProfane(text: string): boolean;
-//     export function add(words: string[]): void;
-//     export function remove(words: string[]): void;
-//     export function list(): string[];
-//     export function clean(text: string, replaceWith?: string): string;
-//   }
-  
