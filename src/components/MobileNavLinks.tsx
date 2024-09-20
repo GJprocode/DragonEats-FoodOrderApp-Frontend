e0c6addc5aaa07f0127060d6c32aa7f8af0c099d
Gert Johannes Fourie <132646907+GJprocode@users.noreply.github.com>
2024-09-20 15:23:49 +0700

    fixed JWT ADMIN, move to 12h18min checkoutbutton (#3)

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -1,6 +1,7 @@
 // C:\Users\gertf\Desktop\FoodApp\frontend\src\components\MobileNavLinks.tsx
 
-import React, { useEffect, useState } from 'react';
+
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './ui/button';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -9,51 +10,47 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Ensure this is defined in your .env file
 
 const MobileNavLinks = () => {
-  const { logout, user } = useAuth0();
+  const { logout, user, getAccessTokenSilently } = useAuth0();
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const checkAdminRole = async () => {
       try {
         if (user?.email) {
-          // Fetch admin status from the backend
-          const response = await axios.get(`${API_BASE_URL}/check-admin/${user.email}`);
+          const token = await getAccessTokenSilently();
+          const response = await axios.get(`${API_BASE_URL}/api/admin/check-admin/${user.email}`, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
           setIsAdmin(response.data.isAdmin);
         }
       } catch (error) {
         console.error('Error checking admin role:', error);
+        // This will help you understand the nature of the error
+        if (axios.isAxiosError(error)) {
+          console.error('Axios error response:', error.response);
+        }
       }
     };
 
     checkAdminRole();
-  }, [user]);
+  }, [user, getAccessTokenSilently]);
 
   return (
     <>
-      <Link
-        to="/manage-restaurant"
-        className="flex bg-white items-center font-bold hover:text-green-500"
-      >
+      <Link to="/manage-restaurant" className="flex bg-white items-center font-bold hover:text-green-500">
         Manage Restaurant
       </Link>
-      <Link
-        to="/user-profile"
-        className="flex bg-white items-center font-bold hover:text-green-500"
-      >
+      <Link to="/user-profile" className="flex bg-white items-center font-bold hover:text-green-500">
         User Profile
       </Link>
       {isAdmin && (
-        <Link
-          to="/admin-panel"
-          className="flex bg-white items-center font-bold hover:text-green-500"
-        >
+        <Link to="/admin-panel" className="flex bg-white items-center font-bold hover:text-green-500">
           Admin
         </Link>
       )}
-      <Button
-        onClick={() => logout()}
-        className="flex items-center px-3 font-bold hover:bg-gray-500"
-      >
+      <Button onClick={() => logout()} className="flex items-center px-3 font-bold hover:bg-gray-500">
         Log Out
       </Button>
     </>
