435582bcfe5151e4f377ebdfb8f879d9770810fd
Gert Johannes Fourie <132646907+GJprocode@users.noreply.github.com>
2024-10-07 20:49:24 +0700

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="bg-green-600 py-10">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between text-center md:text-left">
         <span className="text-sm text-white font-bold tracking-tight mb-4 md:mb-0">
-          © 2024 Harmony Agri Solutions. All rights reserved.
+          © 2024 DragonEats&Drinks.com. All rights reserved.
         </span>
         <span className="text-white font-bold tracking-tight flex gap-4">
           <Link to="/privacy-policy">Privacy Policy</Link>
